4ebf47490ade107f67cdec10d49a17a9eb9bb287
Hoang Nguyen <hoangmegamind1302@gmail.com>
2024-03-03 15:15:46 +0700

    take only id and username from session

--- src/routes/web.route.js
+++ src/routes/web.route.js
@@ -2,14 +2,22 @@ var router = require('express').Router();
 const login = require('../controllers/login.controller');
 const register = require('../controllers/register.controller');
 const authMiddleware = require('../middlewares/auth.middlewares');
+const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
 
 module.exports = app => {
     // router.get('/auth/profile', authMiddleware.loggedin, (req, res) => {
     //     res.status(200).json({ message : 'hello' });
     // });
 
-    router.get('/auth/profile', (req, res) => {
-        res.status(200).json({ message: 'hello' });
+    router.get('/auth/profile', authMiddleware.loggedin, (req, res) => {
+        console.log(req.session.user);
+        if (req.session.loggedin) {
+            const { userid, username } = req.session.user; // Lấy id và username từ session
+            return res.status(200).json({ userid, username });
+        } else {
+            return res.status(401).json({ message: 'You are not logged in!' });
+        }
     });
     
     app.use(router);
